a9e0572583d76e4bb6eb9824d1fb85f0478ad9ce
Abigail Edwards <63216164+aedward8@users.noreply.github.com>
2021-02-05 13:56:42 -0800

    Update 4.js

--- challenges/review/4__js-fundamentals/4.js
+++ challenges/review/4__js-fundamentals/4.js
@@ -1,3 +1,14 @@
 // Create a function that calculates how many minutes have elapsed from midnight until right now.
 // The function should still return an accurate answer,
 // even if you ran it a few minutes or hours later.
+
+function minsSinceMidnight() {
+  let current = new Date();
+  let midNight = new Date();
+  //set to nearest midnight in the past
+  midNight.setHours(0,0,0,0); 
+  
+  return Math.floor((current-midNight)/(1000/60))
+}
+
+console.log(minsSinceMidnight());
